33aed9aa63ebee3120047b582c23aea17f040894
Antonin Novak <mail@ant-novak.com>
2022-09-13 23:32:03 +0200

    comments added

--- Transducers Measurements/Lectures/IMDEA_01_sine.py
+++ Transducers Measurements/Lectures/IMDEA_01_sine.py
@@ -2,20 +2,22 @@ import numpy as np
 import sounddevice as sd
 import matplotlib.pyplot as plt
 
-# sample rate [Hz]
-fs = 48000
+''' sample rate '''
+fs = 48000  # [Hz]
 
+''' Devices (Sounddevice) '''
 print(sd.query_devices())
 sd.default.device = (3, 3)
 print(sd.query_devices())
 
-# sin-wave definition
+''' Sine-wave generation '''
 f0 = 553  # [Hz]
 T = 2  # duration of sine wave [s]
 
 t = np.arange(0, T*fs) / fs
 x = np.sin(2*np.pi * f0 * t)
 
+''' Playing and Recording '''
 y = sd.playrec(x,  # input signal
                samplerate=fs,
                channels=1,
@@ -25,13 +27,17 @@ y = sd.playrec(x,  # input signal
 y = y[int(-fs):, 0]  # taking just the last 1 second (fs samples)
 print(y.shape)
 
+''' Fourier Transform '''
 Y = np.fft.rfft(y) / len(y) * 2
 f_axis = np.fft.rfftfreq(len(y), 1/fs)
 
+''' Plot Results '''
 fig, ax = plt.subplots()
 ax.plot(y)
+ax.set(title='output signal', xlabel='samples [-]')
 
 fig, ax = plt.subplots()
 ax.plot(f_axis, 20*np.log10(np.abs(Y)))
+ax.set(xlabel='Frequency [Hz]', ylabel='Magnitude [dB]')
 
 plt.show()
--- Transducers Measurements/Lectures/IMDEA_03_mls.py
+++ Transducers Measurements/Lectures/IMDEA_03_mls.py
@@ -2,14 +2,15 @@ import numpy as np
 import sounddevice as sd
 import matplotlib.pyplot as plt
 
-# sample rate [Hz]
-fs = 48000
+''' sample rate '''
+fs = 48000  # [Hz]
 
+''' Devices (Sounddevice) '''
 print(sd.query_devices())
 sd.default.device = (3, 3)
 print(sd.query_devices())
 
-# MLS definition
+''' MLS definition '''
 M = 15  # order of MLS
 N = 2**M-1  # length of the MLS signal
 
@@ -24,47 +25,47 @@ for n in range(N):
 
 x = 2*mls_signal - 1
 
+''' Fourier Transform '''
 X = np.fft.rfft(x)
 
-fig, ax = plt.subplots()
-ax.plot(np.abs(X))
-
-# fig, ax = plt.subplots()
-# ax.plot(mls_signal)
-
-y = sd.playrec(np.tile(x, 2),  # input signal
+''' Playing and Recording '''
+y = sd.playrec(np.tile(x, 2),  # input signal (repeted twice)
                samplerate=fs,
                channels=1,
                blocking=True)
-
 
 # extract the 1st channel
 y = y[-N:, 0]  # taking just the last 1 second (fs samples)
 print(y.shape)
 
-# calculate the CREST FACTOR
+''' calculate the CREST FACTOR '''
 crest_factor = np.max(np.abs(x)) / np.std(x)
 print(crest_factor)
 
+''' Fourier Transform '''
 X = np.fft.rfft(x) / len(x) * 2
 Y = np.fft.rfft(y) / len(y) * 2
 f_axis = np.fft.rfftfreq(len(y), 1/fs)
 
-# frequency response function
+''' Frequency Response Function '''
 FRF = Y/X
 
-# impulse response
+''' Impulse Response '''
 IR = np.fft.irfft(FRF)
 
+''' Plot '''
 fig, ax = plt.subplots()
 ax.plot(IR)
-ax.set(title='Impulse Response')
+ax.set(title='Impulse Response', xlabel='samples [-]')
 
 fig, ax = plt.subplots()
 ax.semilogx(f_axis, 20*np.log10(np.abs(Y)))
+ax.set(title='Output Power Spectrum',
+       xlabel='Frequency [Hz]', ylabel='Magnitude [dB]')
 
 fig, ax = plt.subplots()
 ax.semilogx(f_axis, 20*np.log10(np.abs(FRF)))
-
+ax.set(title='Frequency Response Function',
+       xlabel='Frequency [Hz]', ylabel='Magnitude [dB]')
 
 plt.show()
